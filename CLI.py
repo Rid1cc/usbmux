e4cc880b87fa60d5cc03d0a5346c87416a7a15bf
Razny <Franciszek.Razny@aptiv.com>
2022-11-09 13:16:50 +0100

    Reboot added

--- CLI.py
+++ CLI.py
@@ -2,3 +2,4 @@ import argparse
 
 ARGPARSER = argparse.ArgumentParser(description="Interface for connected muxes.")
 ARGPARSER.add_argument("--inf", help="Prints device info. [INF-port_name]")
+ARGPARSER.add_argument("--reboot", help="Reboots device. [Reboot-port_name]")
