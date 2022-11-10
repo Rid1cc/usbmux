090f2d6be2e20e17c6f7bb8ee77fc91d9920efdc
Jabrocki <Jan.Jabrocki@aptiv.com>
2022-11-10 12:25:17 +0100

    Format

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import serial.tools.list_ports
+
+
 from time import sleep
 from enum import Enum
 
+
 class off_on(Enum):
+
+    """Enum for off and on
+
+    Args:
+        Enum (str): enum for off and on
+    """
     ON = '1'
     OFF = '0'
 
 
+def check_mux_inf(port_name: str):
 
-def checkMuxInf(port_name: str):
+    """Returns info message
+
+    Args:
+        port_name (str): port name of port where MUX is connected
+
+    Returns:
+        list: returns info message
+    """
     with serial.Serial(
             port_name,
             baudrate=115200,
@@ -22,8 +39,10 @@ def checkMuxInf(port_name: str):
             ser.reset_output_buffer()
             ser.write(b'inf\n')
             sleep(1)
-            data = ser.read(ser.in_waiting)  # read all input buffer
-            lines = data.decode('UTF-8').split('\r\n')  # decode bytes data into string and split lines
+            # read all input buffer
+            data = ser.read(ser.in_waiting)
+            # decode bytes data into string and split lines
+            lines = data.decode('UTF-8').split('\r\n')
             return lines
 
         except Exception as err:
@@ -32,7 +51,13 @@ def checkMuxInf(port_name: str):
             return []
 
 
-def muxReboot(port_name: str):
+def mux_reboot(port_name: str):
+
+    """Reboots MUX
+
+    Args:
+        port_name (str): port name of port where MUX is connected
+    """
     with serial.Serial(
             port_name,
             baudrate=115200,
@@ -53,7 +78,13 @@ def muxReboot(port_name: str):
             print()
 
 
-def change_muxName(port_name:str, mux_name:str):
+def change_mux_name(port_name: str, mux_name: str):
+
+    """Changes MUX name
+    Args:
+        port_name (str): port name of port where MUX is connected
+        mux_name (str): new name of MUX
+    """
     with serial.Serial(
             port_name,
             baudrate=115200,
@@ -65,7 +96,7 @@ def change_muxName(port_name:str, mux_name:str):
         try:
             ser.reset_input_buffer()
             ser.reset_output_buffer()
-            test =f'n,{mux_name}\n'
+            test = f'n,{mux_name}\n'
             ser.write(bytes(test, encoding='utf-8'))
             sleep(1)
             print("RENAMED")
@@ -74,7 +105,16 @@ def change_muxName(port_name:str, mux_name:str):
             print(err, f"happened at port {port_name}")
             print()
 
-def switchRelay(port_name:str, relay_id:str, relay_state:off_on):
+
+def switch_relay(port_name: str, relay_id: str, relay_state: off_on):
+
+    """Switches relay state(on/off)
+
+    Args:
+        port_name (str): port name of port where MUX is connected
+        relay_id (str): id of relay
+        relay_state (off_on): relay state in enum off_on
+    """
     with serial.Serial(
             port_name,
             baudrate=115200,
@@ -86,7 +126,7 @@ def switchRelay(port_name:str, relay_id:str, relay_state:off_on):
         try:
             ser.reset_input_buffer()
             ser.reset_output_buffer()
-            power =f'pwr,{relay_id},{relay_state.value}\n'
+            power = f'pwr,{relay_id},{relay_state.value}\n'
             ser.write(bytes(power, encoding='utf-8'))
             sleep(1)
             if relay_state == off_on.ON:
@@ -97,7 +137,17 @@ def switchRelay(port_name:str, relay_id:str, relay_state:off_on):
             print(err, f"happened at port {port_name}")
             print()
 
-def get_name(port_name:str):
+
+def get_name(port_name: str):
+
+    """Returns name of MUX
+
+    Args:
+        port_name (str): port name of port where MUX is connected
+
+    Returns:
+        str: returns name of MUX
+    """
     with serial.Serial(
             port_name,
             baudrate=115200,
@@ -114,7 +164,7 @@ def get_name(port_name:str):
             data = ser.read(ser.in_waiting)  # read all input buffer
             lines = data.decode('UTF-8').split('\r\n')
             line = lines[3]
-            if line[0:5]=="Name:":
+            if line[0:5] == "Name:":
                 return line[6:]
             else:
                 return 'no_name'
@@ -124,12 +174,9 @@ def get_name(port_name:str):
             return 'error'
 
 
-
-
 if __name__ == '__main__':
-    change_muxName('COM4', 'default')
-    switchRelay('COM4','1', off_on.ON)
+    change_mux_name('COM4', 'default')
+    switch_relay('COM4', '1', off_on.ON)
     print(get_name('COM4'))
-    muxReboot('COM4')
-
-
+    print(check_mux_inf('COM4'))
+    mux_reboot('COM4')
