a9cc5b3293fc35c20698076b3a101c864468779f
Rid1cc <Franciszek.Razny@aptiv.com>
2022-11-10 09:12:58 +0100

    ok

--- CLI.py
+++ CLI.py
@@ -1,6 +1,6 @@
 import argparse
 
 ARGPARSER = argparse.ArgumentParser(description="Interface for connected muxes.")
-ARGPARSER.add_argument("--inf", help="Prints device info.", dest='[port_name]')
-ARGPARSER.add_argument("--reboot", help="Reboots device.", dest='[port_name]')
+ARGPARSER.add_argument("--inf", help="Prints device info.")
+ARGPARSER.add_argument("--reboot", help="Reboots device.")
 ARGPARSER.add_argument("--relayname", help="Changes relay name", nargs=2)
